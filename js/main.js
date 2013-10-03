af81c4f649aa136cc2bee093bcd9e08b86e668ed
George Welch <Furiousg3org3@gmail.com>
2013-10-03 18:42:36 -0400

    Adding scrolling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
+	$('a[id^=show]').click(function (e) {
+		e.preventDefault();
+		var page = $(this).attr('data-page');
+		var pageId = $('#' + page);
 
+		pageId.removeClass('hidden');
+		pageId.addClass('show');
+	$('html,body').animate({ scrollTop: pageId.offset().top }); /*Scroll to anchor*/
+		console.log(page);
+	});
 });
